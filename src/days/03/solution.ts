072b539242425fc18992435ed0f9ea4264e81b05
Ola Nguyen Van <olanguyenvan@gmail.com>
2023-12-03 12:13:31 -0300

    Solution for day 03 part 2

--- src/days/03/solution.ts
+++ src/days/03/solution.ts
@@ -4,29 +4,52 @@ type Position = {
   x: number;
   y: number;
 };
-export function part1(input: string) {
-  const parsedInput = parserPart1(input);
 
-  const partNumbers = findPartNumbers(parsedInput);
+type MapValue = {
+  value: number | string;
+  id: number;
+};
+
+type MapNumberValue = {
+  value: number;
+  id: number;
+};
+type Map = MapValue[][];
+export function part1(input: string) {
+  const schematic = parserPart1(input);
+
+  const mapWithFullNumbers = createMapWithFullNumbers(schematic);
+
+  const partNumbers = findPartNumbers(schematic, mapWithFullNumbers);
+
   const result: number = partNumbers.reduce((acc, curr) => acc + curr, 0);
 
   console.log(`Solution to part 1 is ${result}`);
 }
 
 export function part2(input: string) {
-  const parsedInput = parserPart1(input);
+  const schematic = parserPart1(input);
 
-  const partNumbers = findPartNumbers(parsedInput);
+  const mapWithFullNumbers = createMapWithFullNumbers(schematic);
+
+  const partNumbers = findGearRatios(schematic, mapWithFullNumbers);
+
   const result: number = partNumbers.reduce((acc, curr) => acc + curr, 0);
 
   console.log(`Solution to part 2 is ${result}`);
 }
 
-function findPartNumbers(schematic: string[][]): number[] {
-  const partNumbers: number[] = [];
+function createMapWithFullNumbers(schematic: string[][]): Map {
   const height = schematic.length;
   const width = schematic[0].length;
 
+  const map: Map = Array.from(Array(height), () =>
+    Array.from(Array(width), () => {
+      return { value: ".", id: 0 };
+    })
+  );
+
+  let counter = 0;
   for (let lineIndex = 0; lineIndex < height; lineIndex++) {
     let tmpIndex = 0;
     let tmpNumber = 0;
@@ -34,9 +57,9 @@ function findPartNumbers(schematic: string[][]): number[] {
 
     while (tmpIndex <= width) {
       const parsedDigit = parseInt(schematic[lineIndex][tmpIndex]);
-      const isNumber = !isNaN(parsedDigit);
+      const isDigit = !isNaN(parsedDigit);
 
-      if (isNumber) {
+      if (isDigit) {
         if (tmpNumber === 0) {
           startPosition = {
             y: lineIndex,
@@ -44,64 +67,141 @@ function findPartNumbers(schematic: string[][]): number[] {
           };
         }
         tmpNumber = tmpNumber * 10 + parsedDigit;
-      } else {
-        if (tmpNumber !== 0 || tmpIndex == width) {
-          if (
-            hasAdjacentSymbol(schematic, startPosition, {
-              y: lineIndex,
-              x: tmpIndex - 1,
-            })
-          ) {
-            partNumbers.push(tmpNumber);
-          }
+      }
+
+      if ((isDigit && tmpIndex === width) || (tmpNumber !== 0 && !isDigit)) {
+        const endXPosition = isDigit ? tmpIndex : tmpIndex - 1;
+        // fill map with numbers
+        for (let i = startPosition.x; i <= endXPosition; i++) {
+          map[lineIndex][i] = {
+            value: tmpNumber,
+            id: counter,
+          };
         }
+        counter++;
         tmpNumber = 0;
       }
       tmpIndex++;
     }
   }
 
-  return partNumbers;
+  return map;
 }
 
-function hasAdjacentSymbol(
+function findPartNumbers(
   schematic: string[][],
-  start: Position,
-  end: Position
-): boolean {
-  let foundSymbol = false;
+  mapWithFullNumbers: Map
+): number[] {
+  const partNumbers: number[] = [];
+  const height = schematic.length;
+  const width = schematic[0].length;
 
-  const startIndexY = start.y - 1;
-  const endIndexY = end.y + 1;
-  const startIndexX = start.x - 1;
-  const endIndexX = end.x + 1;
+  for (let lineIndex = 0; lineIndex < height; lineIndex++) {
+    for (let widthIndex = 0; widthIndex < width; widthIndex++) {
+      if (isSymbolPart1(schematic[lineIndex][widthIndex])) {
+        const adjacentNumbers = findAdjacentNumbers(mapWithFullNumbers, {
+          x: widthIndex,
+          y: lineIndex,
+        });
+        for (const adjacentNumber of adjacentNumbers) {
+          const number = adjacentNumber.value;
 
-  for (let tmpY = startIndexY; tmpY <= endIndexY; tmpY++) {
-    for (let tmpX = startIndexX; tmpX <= endIndexX; tmpX++) {
-      const pos = { x: tmpX, y: tmpY };
-      if (!isValidPosition(schematic, pos)) {
-        continue;
-      }
-
-      if (isSpecialSymbol(schematic[tmpY][tmpX])) {
-        return true;
+          partNumbers.push(number);
+        }
       }
     }
   }
 
-  return foundSymbol;
+  return partNumbers;
 }
 
-function isValidPosition(schematic: string[][], pos: Position): boolean {
+function findGearRatios(
+  schematic: string[][],
+  mapWithFullNumbers: Map
+): number[] {
+  const gearRatios: number[] = [];
   const height = schematic.length;
   const width = schematic[0].length;
+
+  for (let lineIndex = 0; lineIndex < height; lineIndex++) {
+    for (let widthIndex = 0; widthIndex < width; widthIndex++) {
+      if (isSymbolPart2(schematic[lineIndex][widthIndex])) {
+        let gearRatio = 1;
+        const adjacentNumbers = findAdjacentNumbers(mapWithFullNumbers, {
+          x: widthIndex,
+          y: lineIndex,
+        });
+        const foundIds = new Set();
+        for (const adjacentNumber of adjacentNumbers) {
+          foundIds.add(adjacentNumber.id);
+          const number = adjacentNumber.value;
+
+          gearRatio *= number;
+        }
+
+        if (foundIds.size === 2) {
+          gearRatios.push(gearRatio);
+        }
+      }
+    }
+  }
+
+  return gearRatios;
+}
+
+function findAdjacentNumbers(
+  mapWithFullNumbers: Map,
+  pos: Position
+): MapNumberValue[] {
+  const mapValues: MapNumberValue[] = [];
+  const height = mapWithFullNumbers.length;
+  const width = mapWithFullNumbers[0].length;
+
+  const startIndexY = pos.y - 1;
+  const endIndexY = pos.y + 1;
+  const startIndexX = pos.x - 1;
+  const endIndexX = pos.x + 1;
+
+  const numberAlreadyAdded = Array.from(Array(height * width), () => false);
+
+  for (let tmpY = startIndexY; tmpY <= endIndexY; tmpY++) {
+    for (let tmpX = startIndexX; tmpX <= endIndexX; tmpX++) {
+      const pos = { x: tmpX, y: tmpY };
+      if (!isValidPosition(mapWithFullNumbers, pos)) {
+        continue;
+      }
+
+      const mapValue = mapWithFullNumbers[tmpY][tmpX];
+      const { id, value: adjacentNumber } = mapValue;
+      if (Number.isInteger(adjacentNumber)) {
+        if (!numberAlreadyAdded[id] && typeof adjacentNumber === "number") {
+          numberAlreadyAdded[id] = true;
+          mapValues.push({
+            id,
+            value: adjacentNumber,
+          });
+        }
+      }
+    }
+  }
+
+  return mapValues;
+}
+
+function isValidPosition(mapWithFullNumbers: Map, pos: Position): boolean {
+  const height = mapWithFullNumbers.length;
+  const width = mapWithFullNumbers[0].length;
 
   const { x, y } = pos;
   return x >= 0 && x < width && y >= 0 && y < height;
 }
 
-function isSpecialSymbol(char: string) {
+function isSymbolPart1(char: string) {
   const isNumber = !isNaN(parseInt(char));
 
   return !isNumber && char !== ".";
+}
+
+function isSymbolPart2(char: string) {
+  return char === "*";
 }
